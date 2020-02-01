56de764fe69b4e8ee9975f27a1c345b4377695ab
shiratap <spencerhirata@gmail.com>
2020-02-01 10:08:29 -0800

    Updates

---
+++ precache-manifest.beeb12d3b582a9f13d6810f7ad472f68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ae9542fdb4e68c7047548b8611e0743",
+    "url": "/react-Client/index.html"
+  },
+  {
+    "revision": "3cfabc1be3791eef71a9",
+    "url": "/react-Client/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e134e5ff33497143dfeb",
+    "url": "/react-Client/static/js/2.fa9be03e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-Client/static/js/2.fa9be03e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3cfabc1be3791eef71a9",
+    "url": "/react-Client/static/js/main.b05539bd.chunk.js"
+  },
+  {
+    "revision": "3d8a4e98760e659b23c7",
+    "url": "/react-Client/static/js/runtime-main.98f4bd3f.js"
+  }
+]);
