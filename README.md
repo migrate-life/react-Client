41d0c0cb1d644cf97eea717178db4de50f7edb09
shiratap <spencerhirata@gmail.com>
2020-02-01 09:57:06 -0800

    initial react app

---
+++ README.md
@@ -0,0 +1,11 @@
+#### [Back to Server](https://github.com/migrate-life/MigrateLife2)
+
+## React Client
+
+Start react client:
+
+```
+cd client
+npm i
+npm start
+```
---
+++ src/components/content/Search/Search.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Search() {
+  return (
+    <div class="container">
+      <div class="card">
+        <img
+          class="card-img-top"
+          src="https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fentries%2Ficons%2Ffacebook%2F000%2F000%2F091%2FTrollFace.jpg"
+        />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" class="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Search;
